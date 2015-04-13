047404b22bc629a809c4c8ed346831f01e601bcd
harunkelesoglu <harunkelesoglu_@hotmail.com>
2015-04-13 23:27:50 +0300

    Projenin ilk hali

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'username' => 'root',
+    'password' => 'asdf1234',
+    'charset' => 'utf8',
+];
